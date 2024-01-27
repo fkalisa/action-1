134ab95422a86f272ae035027eae8ee621a626a1
fkalisa <30494790+fkalisa@users.noreply.github.com>
2024-01-27 15:57:37 -0500

    script

---
+++ .github/workflows/ascii-script.sh
@@ -0,0 +1,8 @@
+echo "hello world"
+ls -alrt
+cat README.md
+sudo apt-get install cowsay -y
+cowsay -f dragon "run for cover , I am dragon RAWR ...." >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls -lrta
